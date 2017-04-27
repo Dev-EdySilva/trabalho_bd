731bb8298ecf13ea8cf2ba9eebbc4eb2c8e3f88a
Edigleysson Silva <edigleyssonsilva@outlook.com>
2017-04-27 20:21:35 -0300

    teste

--- sistemaRH/settings.py
+++ sistemaRH/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = 'http://geeksilva.pythonanywhere.com/'
-# STATIC_URL = '/static/'
+# STATIC_URL = 'http://geeksilva.pythonanywhere.com/'
+STATIC_URL = '/home/geeksilva/trabalho_bd/RHIN/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
