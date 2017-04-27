524de81f48caa3ce460b71ad34e42fcbbdd53dbb
Edigleysson Silva <edigleyssonsilva@outlook.com>
2017-04-27 20:12:07 -0300

    Static_root

--- sistemaRH/settings.py
+++ sistemaRH/settings.py
@@ -135,4 +135,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = ''
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATICFILES_DIRS = (
+)
+
