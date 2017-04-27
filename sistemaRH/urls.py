f4b0b6a9a6d7f13570eb03cbd379d28b920790b1
Edigleysson Silva <edigleyssonsilva@outlook.com>
2017-04-27 20:34:21 -0300

    deploy static

--- sistemaRH/urls.py
+++ sistemaRH/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.url.static import static
+
 urlpatterns = [
 	url(r'^', include('RHIN.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
